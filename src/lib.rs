0546a54af2fc70e2d58ee183cb96db86d396c9b8
Tim Ruffing <crypto@timruffing.de>
2017-07-24 18:13:15 +0200

    Switch to serde for (de)serialization of messages

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,12 @@ extern crate byteorder;
 extern crate secp256k1;
 extern crate bytes;
 extern crate tokio_io;
+extern crate tokio_serde_bincode;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate vec_map;
+extern crate futures;
 
 mod solver;
 mod rng;
--- src/messages.rs
+++ src/messages.rs
@@ -1,107 +1,28 @@
-use bytes::BytesMut;
+//! Protocol messages
+//!
+//! All protocol messages are serialized by serde according to the bincode data format. For
+//! transmission on the wire, tokio_io::codec::length_delimited is used to prepend protocol
+//! messages by an additional length header, thereby creating frames.
+
 use secp256k1::key::{PublicKey, SecretKey};
-use secp256k1::Secp256k1;
 use secp256k1::Signature;
-use std::io;
-use tokio_io::codec::{Encoder, Decoder};
+use vec_map::VecMap;
 
-const MAGIC: [u8; 4] = [0x68, 0x1e, 0x58, 0x12];
-
-/// Network messages take the following form:
+/// A protocol message
 ///
-/// | Field   | Length | Description                                                   |
-/// |---------|--------|---------------------------------------------------------------|
-/// | Magic   | 4      | Fixed magic bytes for identifying network messages            |
-/// | header  | 1      | Message header                                                |
-/// | payload | ...    | Message payload                                               |
-///
-#[derive(Clone, PartialEq, Eq, Debug)]
+/// Protocol messages consist of a header and a payload.
+#[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
 pub struct Message {
     header: Header,
     payload: Payload,
 }
 
-impl Message {
-    fn new(header: Header, payload: Payload) -> Message {
-        Message {
-            header: header,
-            payload: payload,
-        }
-    }
-    /// Takes a byte slice and builds a Message. Returns same as MessageCodec::decode plus the
-    /// remaining (unconsumed) buffer.
-    fn decode<'a>(buf: &'a [u8], secp256k1: &Secp256k1) -> io::Result<Option<(Message, &'a [u8])>> {
-        // Magic
-        if buf.len() < 4 {
-            return Ok(None);
-        }
-        let (magic, buf) = buf.split_at(4);
-        if magic[..4] != MAGIC {
-            return Err(io::Error::new(io::ErrorKind::Other, "Wrong magic bytes"));
-        }
-        let (header, buf) = match try!(Header::decode(buf)) {
-            None => return Ok(None),
-            Some((header, buf)) => (header, buf),
-        };
-        let (payload, buf) = match try!(Payload::decode(buf, secp256k1)) {
-            None => return Ok(None),
-            Some((payload, buf)) => (payload, buf),
-        };
-        return Ok(Some((
-            Message {
-                header: header,
-                payload: payload,
-            },
-            buf,
-        )));
-    }
-    fn encode(&self, buf: &mut BytesMut, secp256k1: &Secp256k1) -> io::Result<()> {
-        buf.extend(MAGIC.iter());
-        self.header.encode(buf)?;
-        self.payload.encode(buf, secp256k1)?;
-        Ok(())
-    }
-}
-
-/// Headers take the following form:
-///
-/// | Field            | Length | Description                                            |
-/// |------------------|--------|--------------------------------------------------------|
-/// | protocol_version | 1      | Protocol version                                       |
-///
-#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+#[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
 struct Header {
     protocol_version: u8,
 }
 
-impl Header {
-    fn new(protocol_version: u8) -> Header {
-        Header { protocol_version: protocol_version }
-    }
-    /// Takes a byte slice and builds a Header. Returns same as MessageCodec::decode plus the
-    /// remaining (unconsumed) buffer.
-    fn decode<'a>(buf: &'a [u8]) -> io::Result<Option<(Header, &'a [u8])>> {
-        if buf.len() < 1 {
-            return Ok(None);
-        }
-        let (ver, buf) = buf.split_at(1);
-        return Ok(Some((Header { protocol_version: ver[0] }, buf)));
-    }
-    fn encode(&self, buf: &mut BytesMut) -> io::Result<()> {
-        buf.extend(vec![self.protocol_version]);
-        Ok(())
-    }
-}
-
-/// Payloads take the following form:
-///
-/// | Field   | Length | Description                                                   |
-/// |---------|--------|---------------------------------------------------------------|
-/// | Type    | 1      | Determines which payload type this is                         |
-/// |         |        |     0: KeyExchange                                            |
-/// | Payload | ...    | The actual payload                                            |
-///
-#[derive(Clone, PartialEq, Eq, Debug)]
+#[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
 enum Payload {
     KeyExchange(KeyExchange),
     DcExponential(DcExponential),
@@ -112,274 +33,82 @@ enum Payload {
     Reveal(Reveal),
 }
 
-impl Payload {
-    /// Takes a byte slice and builds a Payload. Returns same as MessageCodec::decode plus the
-    /// remaining (unconsumed) buffer.
-    fn decode<'a>(buf: &'a [u8], secp256k1: &Secp256k1) -> io::Result<Option<(Payload, &'a [u8])>> {
-        if buf.len() < 1 {
-            return Ok(None);
-        }
-        let (payload_type, buf) = buf.split_at(1);
-        match payload_type[0] {
-            0 => return KeyExchange::decode(buf, secp256k1),
-            _ => {
-                return Err(io::Error::new(
-                    io::ErrorKind::Other,
-                    "Unknown payload variant",
-                ))
-            }
-        }
-    }
-    fn encode(&self, buf: &mut BytesMut, secp256k1: &Secp256k1) -> io::Result<()> {
-        match self {
-            &Payload::KeyExchange(ref key_exchange) => key_exchange.encode(buf, secp256k1),
-            _ => unimplemented!(),
-        }
-    }
-}
-
-/// KeyExchange takes the following form:
-///
-/// | Field      | Length | Description                                                        |
-/// |------------|--------|--------------------------------------------------------------------|
-/// | signature  | 64     | Compressed ECDSA signature over ke_pk key using the long term      |
-/// |            |        | public key                                                         |
-/// | ke_pk      | 33     | Compressed ephemeral public key                                    |
-///
-#[derive(Clone, PartialEq, Eq, Debug)]
+#[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
 struct KeyExchange {
     signature: Signature,
     ke_pk: PublicKey,
 }
 
-impl KeyExchange {
-    fn new(signature: Signature, ke_pk: PublicKey) -> KeyExchange {
-        KeyExchange {
-            signature: signature,
-            ke_pk: ke_pk,
-        }
-    }
-    fn decode<'a>(buf: &'a [u8], secp256k1: &Secp256k1) -> io::Result<Option<(Payload, &'a [u8])>> {
-        if buf.len() < 64 + 33 {
-            return Ok(None);
-        }
-        let (signature, buf) = buf.split_at(64);
-        let signature = {
-            match Signature::from_compact(secp256k1, signature) {
-                Ok(sig) => sig,
-                Err(_) => {
-                    return Err(io::Error::new(
-                        io::ErrorKind::Other,
-                        "Signature parsing failed",
-                    ))
-                }
-            }
-        };
-        let (ke_pk, buf) = buf.split_at(33);
-        let ke_pk = {
-            match PublicKey::from_slice(secp256k1, ke_pk) {
-                Ok(pk) => pk,
-                Err(_) => {
-                    return Err(io::Error::new(
-                        io::ErrorKind::Other,
-                        "Public key parsing failed",
-                    ))
-                }
-            }
-        };
-        return Ok(Some((
-            Payload::KeyExchange(KeyExchange::new(signature, ke_pk)),
-            buf,
-        )));
-    }
-
-    fn encode(&self, buf: &mut BytesMut, secp256k1: &Secp256k1) -> io::Result<()> {
-        buf.extend(vec![0x00]);
-        buf.extend(self.signature.serialize_compact(secp256k1).iter());
-        buf.extend(self.ke_pk.serialize_vec(secp256k1, true));
-        Ok(())
-    }
-}
-
-/// DcExponential takes the following form:
-///
-/// | Field      | Length         | Description                                                |
-/// |------------|----------------|------------------------------------------------------------|
-/// | num_msgs   |              4 | Number of messages of unexcluded peers (sanity check)      |
-/// | commitment |             32 | Commitment to all input messages of the sending peer       |
-/// | dc_exp     | num_peers * 16 | DC-net vector (exponential encoding in finite field)       |
-///
-#[derive(Clone, PartialEq, Eq, Debug)]
+#[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
 struct DcExponential {
     commitment: [u8; 32],
     dc_exp: Vec<[u8; 16]>,
 }
 
-/// DcXor takes the following form:
-///
-/// | Field      | Length              | Description                                           |
-/// |------------|---------------------|-------------------------------------------------------|
-/// | ok         |                   1 | Flag indicating that the DcExponential phase was okay |
-/// | dc_xor     | num_peers * msg_len | DC-net vector (simple XOR pad)                        |
-///
-#[derive(Clone, PartialEq, Eq, Debug)]
+#[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
 struct DcXor {
     ok: bool,
     dc_xor: Vec<Vec<u8>>,
 }
 
-/// Blame takes the following form:
-///
-/// | Field      | Length | Description                                                        |
-/// |------------|--------|--------------------------------------------------------------------|
-/// | ke_sk      |     32 | The ephemeral secret key for this run                              |
-///
-#[derive(Clone, PartialEq, Eq, Debug)]
+#[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
 struct Blame {
     ke_sk: SecretKey,
 }
 
-/// Confirm takes the following form:
-///
-/// | Field      | Length | Description                                                        |
-/// |------------|--------|--------------------------------------------------------------------|
-/// | data       |      ? | Application-defined confirmation data                              |
-///
-#[derive(Clone, PartialEq, Eq, Debug)]
+#[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
 struct Confirm {
     data: Vec<u8>,
 }
 
-/// Reveal consists of a field keys, which contain one or several pairs of the following form:
-///
-/// | Field      | Length | Description                                                        |
-/// |------------|--------|--------------------------------------------------------------------|
-/// | peer_index |      4 | Index of peer whose symmetric key is revealed                      |
-/// | key        |     16 | Revealed symmetric key                                             |
-///
-#[derive(Clone, PartialEq, Eq, Debug)]
+#[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
 struct Reveal {
-    keys: Vec<(u32, [u8; 16])>,
-}
-
-/// Codec implementing the Encoder and Decoder trait for Messages.
-pub struct MessageCodec {
-    secp256k1: Secp256k1,
-}
-
-impl Decoder for MessageCodec {
-    type Item = Message;
-    type Error = io::Error;
-
-    /// Takes a byte buffer and returns Ok(Some(Message)) if everything went ok.
-    /// In that case `buf` is truncated by the bytes that have been read from it.
-    /// If the `buf` isn't large enough for the full message to be read, `buf` is
-    /// not mutated and Ok(None) is returned. If there's an error during parsing
-    /// Err() is returned.
-    fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<Message>> {
-        let (msg, remaining_buf_len) = match try!(Message::decode(&buf[..], &self.secp256k1)) {
-            None => return Ok(None),
-            Some((msg, remaining_buf)) => (msg, remaining_buf.len()),
-        };
-        let consumed = buf.len() - remaining_buf_len;
-        buf.split_to(consumed);
-        return Ok(Some(msg));
-    }
-}
-
-impl Encoder for MessageCodec {
-    type Item = Message;
-    type Error = io::Error;
-
-    /// Writes a message to `buf`.
-    fn encode(&mut self, msg: Message, buf: &mut BytesMut) -> io::Result<()> {
-        msg.encode(buf, &self.secp256k1)
-    }
+    keys: VecMap<[u8; 16]>,
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use secp256k1;
+    use secp256k1::Secp256k1;
     use secp256k1::key::SecretKey;
 
-    #[test]
-    fn decode() {
-        let mut codec = MessageCodec { secp256k1: Secp256k1::new() };
-        let mut buf = BytesMut::new();
-
-        // Not enough bytes available to determine if magic is correct
-        buf.extend([0].iter());
-        assert_eq!(codec.decode(&mut buf).unwrap(), None);
-        assert_eq!(buf.len(), 1);
-
-        // Wrong magic
-        buf.clear();
-        buf.extend([0, 0, 0, 0].iter());
-        assert!(codec.decode(&mut buf).is_err());
-
-        // Correct magic, but too short
-        buf.clear();
-        buf.extend(MAGIC.iter());
-        assert_eq!(codec.decode(&mut buf).unwrap(), None);
-        assert_eq!(buf.len(), 4);
-    }
-    #[test]
-    fn decode_keyexchange() {
-        let mut codec = MessageCodec { secp256k1: Secp256k1::new() };
-        let mut buf = BytesMut::new();
-
-        buf.clear();
-        buf.extend(MAGIC.iter());
-        let variant = 0;
-        buf.extend([variant].iter());
-        let protocol_version = 0;
-        buf.extend([protocol_version].iter());
-        let signature: [u8; 64] = [0xab; 64];
-        buf.extend(signature.iter());
-        let ke_pk: [u8; 33] = [0x02; 33];
-        buf.extend(ke_pk.iter());
-        let mut buf_copy = buf.clone();
-        assert!(codec.decode(&mut buf).unwrap().is_some());
-
-        // Verify that an invalid public key `ke_pk` results in an error.
-        let buf_len = buf_copy.len();
-        buf_copy.truncate(buf_len - 1);
-        buf_copy.extend([0x04].iter());
-        assert_eq!(
-            codec
-                .decode(&mut buf_copy)
-                .unwrap_err()
-                .get_ref()
-                .unwrap()
-                .description(),
-            "Public key parsing failed"
-        );
-    }
+    use std::io::Cursor;
+    use tokio_io::codec::length_delimited;
+    use tokio_serde_bincode::{ReadBincode, WriteBincode};
+    use futures::sink::Sink;
+    use futures::{Future, Stream};
 
     #[test]
-    fn roundtrip() {
-        // Create secret key, public key, message and signature
+    fn roundtrip_serde_bincode() {
+        // Create secret key, public key, message digest and signature
         let secp256k1 = Secp256k1::new();
         let slice: [u8; 32] = [0xab; 32];
         let sk = SecretKey::from_slice(&secp256k1, &slice).unwrap();
         let ke_pk = PublicKey::from_secret_key(&secp256k1, &sk).unwrap();
         assert!(ke_pk.is_valid());
         let slice: [u8; 32] = [0x01; 32];
-        let msg = secp256k1::Message::from_slice(&slice).unwrap();
+        let msg = ::secp256k1::Message::from_slice(&slice).unwrap();
         let sig = secp256k1.sign(&msg, &sk).unwrap();
 
-        // Create Message and encode and decode
-        let msg = Message::new(
-            Header::new(0),
-            Payload::KeyExchange(KeyExchange::new(sig, ke_pk)),
-        );
-        let mut codec = MessageCodec { secp256k1: Secp256k1::new() };
-        let mut buf = BytesMut::new();
-        codec.encode(msg.clone(), &mut buf).unwrap();
-        codec.encode(msg.clone(), &mut buf).unwrap();
-        assert_eq!(codec.decode(&mut buf).unwrap().unwrap(), msg);
-        assert_eq!(codec.decode(&mut buf).unwrap().unwrap(), msg);
-        assert_eq!(buf.len(), 0);
+        // Create message
+        let msg1 = Message {
+            header: Header { protocol_version: 0 },
+            payload: Payload::KeyExchange(KeyExchange {
+                signature: sig,
+                ke_pk: ke_pk,
+            }),
+        };
+
+        // Write message
+        let pipe = Cursor::new(Vec::new());
+        let write = WriteBincode::new(length_delimited::FramedWrite::new(pipe));
+        let write = write.send(msg1.clone()).wait().unwrap();
+
+        // Read message again
+        let pipe = Cursor::new(write.into_inner().into_inner().into_inner());
+        let read = ReadBincode::<_ ,Message>::new(length_delimited::FramedRead::new(pipe));
+        let msg2 = read.wait().next().unwrap().unwrap();
+
+        assert_eq!(msg1, msg2);
     }
 }
